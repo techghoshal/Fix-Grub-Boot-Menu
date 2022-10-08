fb45bd23f13ba47b2d2356d13c7b756887f88c70
aonetech <aonetecha@gmail.com>
2022-10-08 04:53:24 +0100

    grub

--- grub.sh
+++ grub.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #Fix: Windows not showing in grub boot menu | kali Linux
 
+
+tput bold
+tput setaf 40
+tput blink
+echo "$* **FIX GRUB BOOT MENU"
+tput sgr0
+tput sgr0
+tput sgr0
 echo
-fdisk -l
-sleep 1
 echo
 os-prober
 cp grub /etc/default/grub
@@ -11,12 +17,14 @@ echo
 update-grub
 sleep 1
 echo 
+tput setaf 40
 echo -ne '#####                     (33%)\r'
 sleep 1
 echo -ne '#############             (66%)\r'
 sleep 1
 echo -ne '#######################   (100%)\r'
 echo -ne '\n'
+tput sgr0
 echo 
 read -p "> Do you want to restart the computer? Y/[N]: " restart_computer
 if [ $restart_computer == y ]
